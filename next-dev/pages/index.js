e13ae7615ee7c4ffc78c4fe0819a5b3a8cbbe4c3
BlackMG27 <oike279@gmail.com>
2021-12-23 14:27:18 -0500

    completed initial setup

---
+++ next-dev/pages/index.js
@@ -0,0 +1,16 @@
+import React , {Fragment} from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+          <title>NextField Demo</title>
+      </Head>
+      <header>
+        <h1>Temp Title</h1>
+      </header>
+    </Fragment>
+  )
+}
